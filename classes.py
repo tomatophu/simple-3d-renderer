2d37bf6ac0988d9dd2f9cabefd9ed681d0003c5e
Tomatofu <99457089+tomatophu@users.noreply.github.com>
2025-03-06 00:12:59 -0800

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,116 @@
+import math
+from typing import Self
+from numbers import Real
+from collections.abc import Sequence
+
+import pygame as pg
+
+
+class Map(object):
+    def __init__(self: Self,
+                 *points: Sequence[Sequence[Real]],
+                 connections: Sequence[Sequence[int]]=[]):
+
+        self._points = points
+        self._connections = connections
+
+    @property
+    def connections(self: Self):
+        return self._connections
+
+    @connections.setter
+    def connections(self: Self, value: Sequence[Sequence[int]]):
+        self._connections = value
+    
+    @property
+    def points(self: Self):
+        return self._points
+
+    @points.setter
+    def points(self: Self, value: Sequence[Sequence[Real]]):
+        self._points = value
+
+
+class Camera(object):
+    def __init__(self: Self):
+        # x, y, z
+        self._pos = [0, 0, 0]
+        # yaw, pitch, roll (radians)
+        self._dir = [0, 0, 0]
+    
+    @property
+    def pos(self: Self):
+        return self._pos
+
+    @pos.setter
+    def pos(self: Self, value: list[Real]):
+        self._pos = value
+
+    @property
+    def dir(self: Self):
+        return self._dir
+
+    @dir.setter
+    def dir(self: Self, value: list[Real]):
+        self._dir = value
+    
+    def render(self: Self,
+               surf: pg.Surface,
+               points: Map,
+               radius: Real=4,
+               color: pg.Color=pg.Color('white'),
+               fov: Real=90):
+        surf_size = surf.get_size()
+        semisize = (surf_size[0] / 2, surf_size[1] / 2)
+        rot_points = []
+        proj_points = []
+        
+        #fov is up and down (you can see more side-to-side)
+        fov_mult = 1 / math.sin(math.radians(fov / 2))
+
+        for point in points.points:
+            rel_vector = pg.math.Vector3(point[0] - self._pos[0],
+                                         point[1] - self._pos[1],
+                                         point[2] - self._pos[2])
+            
+            # rotation
+            rel_vector.rotate_y_rad_ip(self._dir[0])
+            rel_vector.rotate_x_rad_ip(self._dir[1])
+            rel_vector.rotate_z_rad_ip(self._dir[2])
+
+            # Matrix Multiplication (self-implementation) (yaw & pitch only)
+            """
+            trig = (math.cos(self._dir[0]), math.sin(self._dir[0]))
+            vector_old = rel_vector.copy()
+            rel_vector[0] = trig[0] * vector_old[0] + trig[1] * vector_old[2]
+            rel_vector[2] = -trig[1] * vector_old[0] + trig[0] * vector_old[2]
+            
+            trig = (math.cos(self._dir[1]), math.sin(self._dir[1]))
+            vector_old = rel_vector.copy()
+            rel_vector[1] = trig[0] * vector_old[1] - trig[1] * vector_old[2]
+            rel_vector[2] = trig[1] * vector_old[1] + trig[0] * vector_old[2]
+            """
+
+            rot_points.append(list(rel_vector))
+
+            ratios = [100, 100]
+            if rel_vector[2]:
+                ratios = (rel_vector[0] / rel_vector[2],
+                          rel_vector[1] / rel_vector[2])
+
+            proj_points.append((ratios[0] * fov_mult
+                                * semisize[1] + semisize[0],
+                                -ratios[1] * fov_mult
+                                * semisize[1] + semisize[1]))
+            
+            if rel_vector[2] > 0:
+                pg.draw.aacircle(surf, color, proj_points[-1], radius)
+
+        for connection in points.connections:
+            
+            if (rot_points[connection[0]][2] > 0
+                and rot_points[connection[1]][2] > 0):
+                pg.draw.aaline(surf, color,
+                               proj_points[connection[0]],
+                               proj_points[connection[1]])
+
---
+++ main.py
@@ -0,0 +1,131 @@
+import sys
+import math
+import time
+from typing import Self
+
+import pygame as pg
+
+from classes import Camera
+from classes import Map
+
+WHITE = pg.Color('white')
+BLACK = pg.Color('black')
+
+class Game(object):
+    def __init__(self: Self):
+        pg.init()
+        self._screen_size = (1280, 720)
+
+        self._running = 0
+        self._game_speed = 60
+        self._screen = pg.display.set_mode(self._screen_size,
+                                           flags=pg.RESIZABLE | pg.SCALED,
+                                           vsync=1)
+        pg.display.set_caption('3D Renderer')
+        self._clock = pg.time.Clock()
+
+        self._camera = Camera()
+
+        self._map = Map(
+            (-0.5, -0.5, 1),
+            (0.5, -0.5, 1),
+            (-0.5, -0.5, 2),
+            (0.5, -0.5, 2),
+            (-0.5, 0.5, 1),
+            (0.5, 0.5, 1),
+            (-0.5, 0.5, 2),
+            (0.5, 0.5, 2),
+
+            (1.5, 0, 1.5),
+            (2.5, 0, 1.5),
+            (2, 0, 2),
+            (2, 0, 1),
+            (2, -0.5, 1.5),
+            (2, 0.5, 1.5),
+
+            connections=[
+                (0, 2),
+                (1, 3),
+                (2, 3),
+                (0, 1),
+                (4, 6),
+                (5, 7),
+                (6, 7),
+                (4, 5),
+                (0, 4),
+                (1, 5),
+                (2, 6),
+                (3, 7),
+
+                (8, 9),
+                (10, 11),
+                (12, 13),
+            ]
+        )
+        self._font = pg.font.SysFont('helvetica', 20)
+
+    def run(self: Self):
+        self._running = 1
+        start_time = time.time() 
+        while self._running:
+            
+            delta_time = time.time() - start_time
+            start_time = time.time()
+
+            rel_game_speed = delta_time * self._game_speed
+
+            keys = pg.key.get_pressed()
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self._running = 0
+                elif event.type == pg.KEYDOWN and event.key == pg.K_r:
+                    if keys[pg.K_LMETA] or keys[pg.K_RMETA]:
+                        self._camera.pos = [0, 0, 0]
+                    self._camera.dir = [0, 0, 0]
+
+            # relative x, y, z
+            mvt = (keys[pg.K_w] - keys[pg.K_s],
+                   keys[pg.K_SPACE] - keys[pg.K_LSHIFT],
+                   keys[pg.K_a] - keys[pg.K_d],
+                   keys[pg.K_LEFT] - keys[pg.K_RIGHT],
+                   keys[pg.K_UP] - keys[pg.K_DOWN],
+                   keys[pg.K_QUOTE] - keys[pg.K_RETURN])
+
+            self._camera.pos[1] += mvt[1] * 0.025 * rel_game_speed
+            
+            trig = (math.cos(self._camera.dir[0] - math.pi / 2), 
+                    math.sin(self._camera.dir[0] - math.pi / 2))
+            self._camera.pos[0] -= mvt[0] * trig[0] * 0.025 * rel_game_speed
+            self._camera.pos[2] -= mvt[0] * trig[1] * 0.025 * rel_game_speed
+            self._camera.pos[0] -= mvt[2] * -trig[1] * 0.025 * rel_game_speed
+            self._camera.pos[2] -= mvt[2] * trig[0] * 0.025 * rel_game_speed
+            # remember: cosine is an odd function, sine is an even function
+
+            self._camera.dir[0] += math.radians(mvt[3]) * rel_game_speed
+            self._camera.dir[1] += math.radians(mvt[4]) * rel_game_speed
+            self._camera.dir[2] += math.radians(mvt[5]) * rel_game_speed
+ 
+            self._screen.fill(BLACK)
+            self._camera.render(self._screen, self._map, 2)
+            
+            text = [
+                f'x: {self._camera.pos[0]}',
+                f'y: {self._camera.pos[1]}',
+                f'z: {self._camera.pos[2]}',
+                f'Yaw: {self._camera.dir[0]} rad',
+                f'Pitch: {self._camera.dir[1]} rad',
+                f'Roll: {self._camera.dir[2]} rad',
+            ]
+
+            for dex, line in enumerate(text):
+                pos = (10, 10 + dex * (self._font.point_size + 5))
+                self._screen.blit(self._font.render(line, 1, WHITE), pos)
+
+            pg.display.update()
+
+        pg.quit()
+
+
+if __name__ == '__main__':
+    Game().run()
